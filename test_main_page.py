694f5356a8a22bed93231f99afe40fdc22ddff7c
kazantsev-sergey <bbmm@bk.ru>
2020-02-06 23:31:33 +0700

    Updated test_guest_can_go_to_login_page
    Added check that a user appeared on on the login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -6,6 +7,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page = LoginPage(browser, link)
+    page.should_be_login_page()
+
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
