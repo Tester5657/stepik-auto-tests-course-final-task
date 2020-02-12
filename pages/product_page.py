339810cd1f79a05e16ef7dd30f5e81be38d87024
kazantsev-sergey <bbmm@bk.ru>
2020-02-12 12:37:04 +0700

    Changed verification methods in the test_guest_can_add_product_to_basket.
    Changed the verification methods so that they take as an argument the name of the product and the price of the product
    Made a method that pulls the text-name and price of the product from the element and returns it.

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,12 +7,16 @@ class ProductPage(BasePage):
     def click_add_to_busket_button(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
 
-    def message_product_was_added_in_the_busket_should_appear(self):
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+    def should_have_correct_product_in_the_message(self, product_name):
         message = self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_WAS_ADDED_TO_BUSKET)
-        assert product_name.text in message.text, "Incorrect product name in the message"
+        assert product_name in message.text, "Incorrect product name in the message"
 
-    def message_your_busket_total_price_should_appear(self):
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+    def should_have_correct_price_in_the_message(self, product_price):
         message = self.browser.find_element(*ProductPageLocators.MESSAGE_YOUR_BASKET_TOTAL_IS_NOW)
-        assert product_price.text in message.text, "Incorrect product price in the message"
+        assert product_price in message.text, "Incorrect product price in the message"
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,13 @@
 from pages.product_page import ProductPage
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
+    product_price = page.get_product_price()
+    product_name = page.get_product_name()
     page.click_add_to_busket_button()
     page.solve_quiz_and_get_code()
-    page.message_your_busket_total_price_should_appear()
-    page.message_product_was_added_in_the_busket_should_appear()
+    page.should_have_correct_product_in_the_message(product_name)
+    page.should_have_correct_price_in_the_message(product_price)
